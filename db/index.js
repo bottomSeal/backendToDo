6b6c7abaedd954939dc38db7a62d072593e8966a
bottomSeal <114884340+bottomSeal@users.noreply.github.com>
2023-02-03 20:48:40 +0300

    Update index.js

--- db/index.js
+++ db/index.js
@@ -2,22 +2,14 @@ const { Sequelize } = require("sequelize");
 
 //Создваем instance Sequelize
 const sequelizeInstance = new Sequelize({
-    //   dialect: "postgres",
-    //   host: "21.23.35.4",
-    //   port: 6000,
-    //   database: "js-todo",
-    //   username: "postgres",
-    //   password: "qwerty10",
 
     dialect: "sqlite",
-    storage: "./sqliteData/database.sqlite", //Путь до файла с данными
+    storage: "./sqliteData/database.sqlite",
 });
 
 const initDB = async () => {
     try {
         await sequelizeInstance.authenticate(); //Авторизация нашей ORM в БД
-        // await sequelize.dropSchema('public', {});
-        // await sequelize.createSchema('public', {});
         await sequelizeInstance.sync(); //Синхронизация МОДЕЛЕЙ
         console.log("Sequelize was initialized");
     } catch (error) {
